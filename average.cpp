067f7837a8ae46847e47f10ec1d7bc7f4d18fcdd
vishnudarsan1508 <73700960+vishnudarsan1508@users.noreply.github.com>
2021-10-04 20:14:59 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,16 @@
+nclude<iostream>
+using namespace std;
+int main()
+{
+float n1,n2,n3,sum,avg;
+	cout<<"losangels@1 ";
+	cin>>n1;
+	cout<<"Enter second no: ";
+	cin>>n2;
+	cout<<"Enter third no: ";
+	cin>>n3;
+	sum=n1+n2+n3;
+	avg=sum/3;
+	cout<<"\n Sum= "<<sum;
+	cout<<"\n Average= "<<avg;
+}
